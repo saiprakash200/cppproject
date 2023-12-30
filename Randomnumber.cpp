61414ba63f2e3b53aca608254ab7b95576abcb80
Sai <natarsaiprakash200@gmail.com>
2023-12-30 13:48:08 +0530

    Adding source files

---
+++ Randomnumber.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    srand(time(NULL));
+    int n=rand()%10;
+    cout<<"Random Number generated is : "<<n<<endl;
+    int number,chance=1;
+    do
+    {
+    cout<<"Please enter your guess number"<<endl;
+    cin>>number;
+    if(number>n)
+    {
+        cout<<"Your guess number is high : "<<endl;
+        chance++;
+    }
+    else if(number<n)
+    {
+        cout<<"Your guess number is low : "<<endl;
+        chance++;
+    }
+    else
+    {
+        cout<<"Congratulation you have guessed the correct number in "<<chance<<"chances"<<endl;
+    }
+    }while(number!=n);
+
+}
